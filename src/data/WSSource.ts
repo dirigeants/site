1b96ed6e124c76492da6c44e3ae468879702997a
Jacz <itsmejacz@gmail.com>
2020-03-10 09:41:56 +1100

    Clean github pages branch

--- src/data/WSSource.ts
+++
@@ -1,9 +0,0 @@
-import { DocsSource } from './DocsSource';
-
-const branchBlacklist = new Set(['gh-pages', 'docs']);
-export default new DocsSource({
-	id: 'ws',
-	name: 'WebSocket',
-	repo: 'dirigeants/ws',
-	branchFilter: (branch: string): boolean => !branchBlacklist.has(branch) && !branch.startsWith('dependabot')
-});
