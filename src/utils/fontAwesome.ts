c94cc0a081eccd85cada0c7a80beb4744527d990
Jacz <itsmejacz@gmail.com>
2020-06-06 00:55:01 +1000

    Add search, sun and moon icons to FontAwesome

--- src/utils/fontAwesome.ts
+++ src/utils/fontAwesome.ts
@@ -4,9 +4,9 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 
 // Icons
 import { faFrown } from '@fortawesome/free-regular-svg-icons';
-import { faBook } from '@fortawesome/free-solid-svg-icons';
+import { faBook, faSearch, faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import { faDiscord, faGithub, faNpm, faPatreon } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faDiscord, faGithub, faBook, faFrown, faNpm, faPatreon);
+library.add(faDiscord, faGithub, faBook, faFrown, faNpm, faPatreon, faSearch, faSun, faMoon);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
