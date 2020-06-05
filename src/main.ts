d6f71e1fd7d660839722f98632b5de08d7fee733
Jacz <itsmejacz@gmail.com>
2020-06-06 00:54:23 +1000

    Bring back buefy for now

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
 import HighlightJS from './utils/highlightJS';
 import App from './App.vue';
 import router from './router';
 import './utils/fontAwesome';
 
+Vue.use(Buefy, { defaultIconComponent: 'font-awesome-icon', defaultIconPack: 'fas' });
 Vue.use(HighlightJS);
 
 Vue.config.productionTip = false;
